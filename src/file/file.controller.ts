fb15c6b86e5560f36161f129541afcd581f6ad8f
Kelvinvida99 <kelvinvida99@gmail.com>
2025-01-20 23:21:40 -0400

    new

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,19 +1,55 @@
 import {
+  BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileService } from './file.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { fileFilter } from './helper/fileFilter.helper';
+import { diskStorage } from 'multer';
+import { fileNamer } from './helper';
+import { ConfigService } from '@nestjs/config';
+import { Response } from 'express';
 
 @Controller('files')
 export class FileController {
-  constructor(private readonly fileService: FileService) {}
+  constructor(
+    private readonly fileService: FileService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  @Get('movie/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.fileService.getStaticMoviePost(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('movie')
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: fileFilter,
+      storage: diskStorage({
+        destination: './static/posts',
+        filename: fileNamer,
+      }),
+    }),
+  )
   uploaMovieImage(@UploadedFile() file: Express.Multer.File) {
-    return file;
+    if (!file) {
+      throw new BadRequestException('Make sure that the file is an image');
+    }
+
+    const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
+
+    return { secureUrl };
   }
 }
--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
+
+import { Injectable, BadRequestException } from '@nestjs/common';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+  getStaticMoviePost(imageName: string) {
+    const path = join(__dirname, '../../static/posts', imageName);
+
+    if (!existsSync(path))
+      throw new BadRequestException(`No post found with image ${imageName}`);
+
+    return path;
+  }
+}
